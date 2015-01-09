d0f9757220f8fae327e941ff22de3c559ddbc04a
pjc02478 <pjc0247@gmail.com>
2015-01-09 21:14:04 +0900

    Update source.js

--- lectures/3_change_props/source.js
+++ lectures/3_change_props/source.js
@@ -3,17 +3,17 @@ var HelloWorldLayer = cc.Layer.extend({
   ctor:function () {
     this._super();
 
-		var node = ccs.uiReader.widgetFromJsonFile("res/studio_test_1.json");
-		this.addChild(node);
+    var node = ccs.uiReader.widgetFromJsonFile("res/studio_test_1.json");
+    this.addChild(node);
 		
-		var button = node.getChildByName("button");
-		var label = node.getChildByName("label");
+    var button = node.getChildByName("button");
+    var label = node.getChildByName("label");
 		
-		/* 버튼이 눌렷을 때 */
-		button.addClickEventListener(function(){
-		  /* 라벨을 2배 사이즈로 */
-			label.setScale(2);
-		});
+    /* 버튼이 눌렷을 때 */
+    button.addClickEventListener(function(){
+      /* 라벨을 2배 사이즈로 */
+      label.setScale(2);
+    });
 		
     return true;
   }
