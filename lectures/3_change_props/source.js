29f851c63b1d4a405b9cbfc3593c037d0d44107a
pjc02478 <pjc0247@gmail.com>
2015-01-09 21:12:27 +0900

    Create source.js

---
+++ lectures/3_change_props/source.js
@@ -0,0 +1,29 @@
+
+var HelloWorldLayer = cc.Layer.extend({
+  ctor:function () {
+    this._super();
+
+		var node = ccs.uiReader.widgetFromJsonFile("res/studio_test_1.json");
+		this.addChild(node);
+		
+		var button = node.getChildByName("button");
+		var label = node.getChildByName("label");
+		
+		/* 버튼이 눌렷을 때 */
+		button.addClickEventListener(function(){
+		  /* 라벨을 2배 사이즈로 */
+			label.setScale(2);
+		});
+		
+    return true;
+  }
+});
+
+var HelloWorldScene = cc.Scene.extend({
+  onEnter:function () {
+    this._super();
+    var layer = new HelloWorldLayer();
+    this.addChild(layer);
+  }
+});
+
